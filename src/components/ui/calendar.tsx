a00dcabf01b7c2ed8fa2916eda616d102336a175
gauravK0i <gaurav.koirala@globalsquareit.com>
2025-11-19 17:13:24 +0545

    Fixes for blog

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+// import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -41,12 +41,12 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={
-        ({
-          IconLeft: ({ ..._props }: any) => <ChevronLeft className="h-4 w-4" />,
-          IconRight: ({ ..._props }: any) => <ChevronRight className="h-4 w-4" />,
-        } as any)
-      }
+      // components={
+      //   ({
+      //     IconLeft: ({ ..._props }: any) => <ChevronLeft className="h-4 w-4" />,
+      //     IconRight: ({ ..._props }: any) => <ChevronRight className="h-4 w-4" />,
+      //   } as any)
+      // }
       {...props}
     />
   );
